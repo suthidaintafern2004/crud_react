6c0d6d518946e9363662c9c20f46baed1a158a1d
suthidaintafern2004“ <suthidainta7@gmail.com>
2024-02-20 12:58:34 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    use:"root",
+    host:"localhost",
+    password:"",
+    database:"employeeSystem",
+})
